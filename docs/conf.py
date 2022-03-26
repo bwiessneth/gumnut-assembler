af07eecbbd5bc3b68dfa9252e5fa2ab3f274724a
bwiessneth <b.wiessneth@googlemail.com>
2022-03-26 20:41:22 +0100

    docs: Fix configuration for latest sphinx-rtd-theme version

--- docs/conf.py
+++ docs/conf.py
@@ -68,13 +68,13 @@ html_theme_options = {
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_last_updated_fmt = '%b %d, %Y'
 html_context = {
-    'css_files': [],
     'display_github': True,
     'github_user': 'bwiessneth',
     'github_repo': 'gumnut-assembler',
     'github_version': 'master',
     'conf_py_path': '/docs/'
 }
+html_css_files = []
 html_show_copyright = False
 
 
